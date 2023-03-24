eded89c5e9966bcbe86083bcd7b1e22d107cfe1d
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-03-24 18:25:07 +0300

    Added TopMoversView.

---
+++ SwiftCoin/Core/Home/Views/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  SwiftCoin
+//
+//  Created by Yunus Emre Taşdemir on 24.03.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                // top movers view
+                TopMoversView()
+                
+                //all coin view 
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ SwiftCoin/Core/Home/Views/TopMoversView.swift
@@ -0,0 +1,32 @@
+//
+//  TopMoversView.swift
+//  SwiftCoin
+//
+//  Created by Yunus Emre Taşdemir on 24.03.2023.
+//
+
+import SwiftUI
+
+struct TopMoversView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Top Movers")
+                .font(.headline)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 16) {
+                    ForEach(0 ..< 5, id: \.self) { _ in
+                        TopMoversItemView()
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct TopMoversView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoversView()
+    }
+}
