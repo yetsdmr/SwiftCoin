627a9fc454d6dc454f5bc7c997c58e480dad49f1
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-03-24 17:57:03 +0300

    Initial commit.

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Yunus Emre Taşdemir on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
