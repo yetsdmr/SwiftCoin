654830da752c345c782796aa42ac724efbc5d353
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-03-30 11:52:37 +0300

    Added Kingfisher.

--- SwiftCoin/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftCoin/Core/Home/ViewModels/HomeViewModel.swift
@@ -17,7 +17,8 @@ class HomeViewModel: ObservableObject {
     }
     
     func fetchCoinData() {
-        let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h"
+        //let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h"
+        let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h&locale=en"
         
         guard let url = URL(string: urlString) else {return}
         
--- SwiftCoin/Core/Home/Views/TopMoversItemView.swift
+++ SwiftCoin/Core/Home/Views/TopMoversItemView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct TopMoversItemView: View {
     let coin: Coin
@@ -13,7 +14,7 @@ struct TopMoversItemView: View {
     var body: some View {
         VStack(alignment: .leading) {
             // image
-            Image(systemName: "bitcoinsign.circle.fill")
+            KFImage(URL(string: coin.image))
                 .resizable()
                 .frame(width: 32, height: 32)
                 .foregroundColor(.orange)
